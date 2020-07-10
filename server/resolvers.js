9b150ce23a910773fd6eafc6940616dc19230aca
Max Ince <max@Maxs-MacBook-Pro.local>
2020-07-09 20:45:22 -0700

    s2e15

--- server/resolvers.js
+++ server/resolvers.js
@@ -2,4 +2,9 @@ const db = require('./db');
 const Query = {
     jobs: () => db.jobs.list()
 }
-module.exports = { Query };
+
+const Job = {
+    company: (job) => db.companies.get(job.companyId)
+}
+
+module.exports = { Query, Job };
