e713f67f434eb836dad9fc0bc57b84663d762e65
max ince <max.ince@glassdoor.com>
2020-07-28 20:32:07 -0700

    s3e1

--- server/resolvers.js
+++ server/resolvers.js
@@ -2,8 +2,14 @@ const db = require('./db');
 
 const Query = {
     company: (root, { id }) => db.companies.get(id),
-    job: (root, { id }) => db.jobs.get(id),  
+    job: (root, { id }) => db.jobs.get(id),
     jobs: () => db.jobs.list()
+}
+
+const Mutation = {
+    createJob: (root, { companyId, title, description}) => {
+        return db.jobs.create({ companyId, title, description });
+    }
 }
 
 const Company = {
@@ -15,4 +21,4 @@ const Job = {
     company: (job) => db.companies.get(job.companyId)
 }
 
-module.exports = { Query, Company, Job };
+module.exports = { Query, Mutation, Company, Job };
